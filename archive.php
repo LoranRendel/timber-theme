7eebe9e7e569ea62599372eb0c37d5df0e5a7c80
Loran A. Rendel <loran@xpor.org>
2023-06-07 18:59:49 +0300

    autocomputing templates names for archives based on Wordpress logic

--- archive.php
+++ archive.php
@@ -9,12 +9,14 @@
  *
  * Methods for TimberHelper can be found in the /lib sub-directory
  *
- * @package  WordPress
+ * @package     WordPress
  * @subpackage  Timber
- * @since   Timber 0.2
+ * @since       Timber 0.2
  */
 
-$templates = array( 'archive.twig', 'index.twig' );
+use Timber\Timber;
+
+$templates = [ 'archive.twig', 'index.twig' ];
 
 $context = Timber::context();
 
@@ -29,12 +31,51 @@ if ( is_day() ) {
 	$context['title'] = single_tag_title( '', false );
 } elseif ( is_category() ) {
 	$context['title'] = single_cat_title( '', false );
-	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
 } elseif ( is_post_type_archive() ) {
 	$context['title'] = post_type_archive_title( '', false );
-	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
 }
 
-$context['posts'] = new Timber\PostQuery();
+//region based on template-loader.php
+/** @noinspection DuplicatedCode */
+$tag_templates  = array(
+	'is_embed'             => 'get_embed_template',
+	'is_404'               => 'get_404_template',
+	'is_search'            => 'get_search_template',
+	'is_front_page'        => 'get_front_page_template',
+	'is_home'              => 'get_home_template',
+	'is_privacy_policy'    => 'get_privacy_policy_template',
+	'is_post_type_archive' => 'get_post_type_archive_template',
+	'is_tax'               => 'get_taxonomy_template',
+	'is_attachment'        => 'get_attachment_template',
+	'is_single'            => 'get_single_template',
+	'is_page'              => 'get_page_template',
+	'is_singular'          => 'get_singular_template',
+	'is_category'          => 'get_category_template',
+	'is_tag'               => 'get_tag_template',
+	'is_author'            => 'get_author_template',
+	'is_date'              => 'get_date_template',
+	'is_archive'           => 'get_archive_template',
+);
+$templates_list = [];
+// Loop through each of the template conditionals, and find the appropriate template file.
+$function = function ( $hook, ...$args ) use ( &$templates_list ) {
+	if ( str_ends_with( $hook, '_template' ) ) {
+		$templates = $args[2];
+		foreach ( $templates as $template ) {
+			$templates_list[] = preg_replace( '/\.php$/', '.twig', $template );
+		}
+	}
+
+	return $hook;
+};
+add_action( 'all', $function, 100, 4 );
+foreach ( $tag_templates as $tag => $template_getter ) {
+	if ( call_user_func( $tag ) ) {
+		$template = call_user_func( $template_getter );
+	}
+}
+remove_action( 'all', $function, 100 );
+$templates = array_values( array_unique( [ ...$templates_list, ...$templates ] ) );
+//endregion
 
 Timber::render( $templates, $context );
